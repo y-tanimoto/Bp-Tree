ca690385e49365bbf10e3119d8c98a51e2821cb3
y-tanimoto <tanimoto@asg.cs.tuat.ac.jp>
2022-01-29 16:09:29 +0900

    成功、ただし右ノード優先に

--- main.cpp
+++ main.cpp
@@ -3,37 +3,27 @@
 int main() {
     Tree bp_tree(3);
 
-    bp_tree.add(3);
     bp_tree.add(2);
-    bp_tree.add(1);
-
-    bp_tree.add(4);
+    bp_tree.add(3);
     bp_tree.add(7);
-    bp_tree.add(8);
-    bp_tree.add(5);
-    bp_tree.add(6);
-    bp_tree.print_tree();
-    
-    bp_tree.add(10);
     bp_tree.add(11);
+    bp_tree.add(13);
+    bp_tree.add(17);
+    bp_tree.add(23);
+    bp_tree.add(29);
+    bp_tree.add(31);
+    bp_tree.add(37);
+    bp_tree.add(43);
+    bp_tree.add(47);
+    bp_tree.add(5);
+    bp_tree.add(19);
+    bp_tree.add(41);
     bp_tree.print_tree();
 
-    bp_tree.add(9);
-    bp_tree.add(13);
+    bp_tree.add(10);
     bp_tree.print_tree();
-    bp_tree.add(12);
-    bp_tree.add(0);
-    bp_tree.add(14);
-    bp_tree.add(15);
-    bp_tree.add(16);
-    bp_tree.add(17);
-    bp_tree.add(18);
-    bp_tree.add(19);
-    bp_tree.add(20);
-    bp_tree.add(21);
-    bp_tree.add(22);
-    bp_tree.add(23);
-    bp_tree.add(24);
+
+    bp_tree.add(40);
     bp_tree.print_tree();
 
     return 0;
--- tree.cpp
+++ tree.cpp
@@ -27,6 +27,26 @@ bool Tree::add(const int key_to_add) {
     // 左：ROUNDUP((m+1)/2)個
     int left_hold = ceil((m_node_size+1)/2);
 
+    // キーが入る場所を決定
+    int key_index;
+    int left = std::numeric_limits<int>::min();   // 左側の値の初期値はintの最小値に
+    int right;
+    for (int i=0; i<=target_node->count_keys(); i++) {
+        if (i == target_node->count_keys()) {
+            key_index = i;
+            break;
+        }
+        right = target_node->get_key(i);
+
+        if (left <= key_to_add && key_to_add < right) {
+            key_index = i;
+            break;
+        }
+        left = right;
+    }
+    // キーが左ノード、右ノードのどちらに入るか
+    bool key_in_left = (key_index < left_hold);
+
     // 分割後のノードの用意
     Node *left_node, *right_node;
     // 左の葉ノードは現在のノードをそのまま利用
@@ -34,23 +54,24 @@ bool Tree::add(const int key_to_add) {
     // 右の葉ノードの用意
     right_node = new Node(m_node_size, nullptr);
     // 左ノードのleft_hold番目以降の要素を右ノードに移行
-    for (int i=left_hold; i<left_node->count_keys(); i++) {
+    for (int i=left_hold-key_in_left; i<left_node->count_keys(); i++) {
         right_node->add(left_node->get_key(i));
         std::cout << "move " << left_node->get_key(i) << std::endl;
     }
     // 右ノードに移行した子ノードは左ノードから削除
-    for (int i=left_hold; i<left_node->count_keys(); i++) {
+    for (int i=left_hold-key_in_left; i<left_node->count_keys(); i++) {
         left_node->del(i);
     }
 
     // 左ノードに右ノードへのポインタを設定
     left_node->set_right_node(right_node);
 
-    // 新しい子ノードを登録
-    // 右ノードの最小値と比較
-    if (key_to_add < right_node->get_min_key_recursive()) {
+    // 新しいキーを登録
+    // キーが入る場所(key_index)が左ノード内であれば、左ノードに挿入
+    if (key_in_left) {
         left_node->add(key_to_add);
     }
+    // キーが入る場所(key_index)が右ノード内であれば、右ノードに挿入
     else {
         right_node->add(key_to_add);
     }
@@ -121,6 +142,27 @@ bool Tree::m_add_child_node(Node* parent_node, Node* child_node) {
     // 左：ROUNDUP((m+1)/2)個
     int left_hold = ceil((m_node_size+1)/2);
 
+    // キーが入る場所を決定
+    int key_index;
+    int key = child_node->get_min_key_recursive();
+    int left = std::numeric_limits<int>::min();   // 左側の値の初期値はintの最小値に
+    int right;
+    for (int i=0; i<=parent_node->count_keys(); i++) {
+        if (i == parent_node->count_keys()) {
+            key_index = i;
+            break;
+        }
+        right = parent_node->get_key(i);
+
+        if (left <= key && key < right) {
+            key_index = i;
+            break;
+        }
+        left = right;
+    }
+    // 子ノードが左ノード、右ノードのどちらに入るか
+    bool child_node_in_left = (key_index < left_hold);
+
     // 分割後のノードの用意
     Node *left_node, *right_node;
     // 左ノードは親ノードをそのまま利用
@@ -129,21 +171,22 @@ bool Tree::m_add_child_node(Node* parent_node, Node* child_node) {
     right_node = new Node(m_node_size, left_node->get_child(left_hold), left_node->get_child(left_hold+1), nullptr);
 
     // 左ノードのleft_hold+2番目以降の要素を右ノードに移行
-    for (int i=left_hold+2; i<left_node->count_keys(); i++) {
+    for (int i=left_hold+2-child_node_in_left; i<left_node->count_keys(); i++) {
         left_node->get_child(i)->set_parent(right_node);
         right_node->add(left_node->get_child(i));
     }
     // 右ノードに移行した子ノードは左ノードから削除
-    for (int i=left_node->count_keys(); i>=left_hold; i--) {
+    for (int i=left_node->count_keys(); i>=left_hold-child_node_in_left; i--) {
         left_node->del(i);
     }
 
-    // 新しい子ノードを登録
-    // 右ノードの最小値と比較
-    if (child_node->get_min_key_recursive() < right_node->get_min_key_recursive()) {
+    // 子ノードを登録
+    // 子ノードが入る場所(key_index)が左ノード内であれば、左ノードに挿入
+    if (child_node_in_left) {
         left_node->add(child_node);
         child_node->set_parent(left_node);
     }
+    // 子ノードが入る場所(key_index)が右ノード内であれば、右ノードに挿入
     else {
         right_node->add(child_node);
         child_node->set_parent(right_node);
